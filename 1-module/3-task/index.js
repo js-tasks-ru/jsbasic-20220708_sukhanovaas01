fef59511d56d33c2f4c6575f0d9564739d0f9f4a
Anastasia Sukhanova <sukhanovaas01@gmail.com>
2022-07-14 19:44:45 +0300

    checkSpam

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,10 +1,14 @@
 function ucFirst(str) {
 
-  let str1 = str.charAt(0).toUpperCase() + str.slice(1);
+  let str1 = str
+              .charAt(0)  
+              .toUpperCase() + str.slice(1);
 
   return str1;
   // alert( str.slice(1) ); добавит нам все буквы после первой
   // alert( 'name'[0].toUpperCase() ); // 'n'
   // alert( str.charAt(1000) ); // '' (пустая строка)
 }
-console.log( ucFirst('к') );
+console.log( ucFirst('asdfGHHJJ') );
+
+// в задаче применены методы
