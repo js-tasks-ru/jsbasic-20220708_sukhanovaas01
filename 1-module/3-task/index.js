336059fd652279c55ce45af591dacb431fff9f18
Anastasia Sukhanova <sukhanovaas01@gmail.com>
2022-07-14 17:33:27 +0300

    string solved

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
-  // ваш код...
+
+  let str1 = str.charAt(0).toUpperCase() + str.slice(1);
+
+  return str1;
+  // alert( str.slice(1) ); добавит нам все буквы после первой
+  // alert( 'name'[0].toUpperCase() ); // 'n'
+  // alert( str.charAt(1000) ); // '' (пустая строка)
 }
+console.log( ucFirst('к') );
