e4f1ba218534f21dbf85bf4b710fbee3dc450e31
Anastasia Sukhanova <sukhanovaas01@gmail.com>
2022-07-29 20:38:38 +0300

    4 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let newUl = document.createElement("Ul"); // первично создадим ul
+  for (let friend of friends) {
+    newUl.insertAdjacentHTML(
+      "beforeEnd",
+      `<li>${friend.firstName} ${friend.lastName}</li>`
+    );
+  }
+
+  return newUl;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.rows) {
+    console.log(tr);
+    if (tr.cells[1].innerHTML < "18") {
+      tr.style.textDecoration = "line-through";
+    }
+    if (tr.cells[3].dataset.available === "true") {
+      tr.classList.add("available");
+    } else if (tr.cells[3].dataset.available === "false") {
+      tr.classList.add("unavailable");
+    } else {
+      tr.hidden = true;
+    }
+    if (tr.cells[2].innerHTML === "f") {
+      tr.classList.add("female");
+    } else if (tr.cells[2].innerHTML === "m") {
+      tr.classList.add("male");
+    }
+  }
 }
