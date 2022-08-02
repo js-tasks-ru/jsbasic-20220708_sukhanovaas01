7f5b81c8687920894906aa040d48052094663508
Anastasia Sukhanova <sukhanovaas01@gmail.com>
2022-08-02 21:17:05 +0300

    5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,12 +2,8 @@ function toggleText() {
   document.addEventListener("click", (event) => {
     let button = document.querySelector(".toggle-text-button");
     if (event.target == button) {
-      console.log(event.target == button);
-      console.log(button);
       let text = document.querySelector("#text");
       text.hidden = !text.hidden;
-    } else {
-      return;
     }
   });
 }
